f1c7bf9211089c5fb905c1bed6ddb215b951667b
Luchoblas201 <luis.mr92@hotmail.com>
2026-05-09 20:23:39 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'xr150-v1';
+const ASSETS = [
+  './honda-xr150-aceite.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
